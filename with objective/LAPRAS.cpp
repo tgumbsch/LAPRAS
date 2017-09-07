199573266da2483b2b5b8aaaccaa5e73d1d890bf
Gumbsch  Thomas <tgumbsch@bs-mbpr235.d.ethz.ch>
2017-09-07 16:49:50 +0200

    New folder structure

---
+++ with objective/LAPRAS.cpp
@@ -0,0 +1,41 @@
+
+class LAPRAS {
+    std::vector<int> S;
+    std::vector<int> best_index;
+    double best_obj;
+    int new_k;
+    std::vector<std::vector<double>> Distances;
+    bool dist;
+
+
+public:
+    void Init(std::vector<int> &Initial);
+    void Fit(std::vector<int> &Node, int k);
+};
+
+void LAPRAS::Init(std::vector<std::vector<double>> &Dist){
+    S.resize(Dist.size());
+    std::iota(S.begin(), S.end(),1);
+    Distances = Dist;
+    best_index = S;
+    best_obj = 0;
+    for(int i = 0; i< S.size()-1;i++){
+        best_obj += Dist[i][i+1];
+    }
+}
+
+void LAPRAS::Fit(std::vector<int> &Node, int k, double val){
+    for(int tilde_k = 0; tilde_k < Node.size()-k; tilde_k++){
+        new_k = tilde_k + k;
+        int save = Node[new_k];
+        double obj =
+        Node.erase(Node.begin()+new_k);
+        if (Node.size()>0){
+                Fit(Node,new_k);
+        }
+        Node.insert(Node.begin()+new_k,save);
+
+
+    }
+
+}
---
+++ without objective/PowerSet.cpp
@@ -0,0 +1,75 @@
+#include <math.h>
+
+void PowerSet(std::vector<int> Set)
+{
+    /*set_size of power set of a set with set_size
+      n is (2**n -1)*/
+    int set_size = Set.size();
+    unsigned int pow_set_size = pow(2, set_size);
+    int counter, j,k;
+    std::vector<int> Processing(set_size);
+
+    /*Run from counter 000..0 to 111..1*/
+    for(counter = 0; counter < pow_set_size; counter++)
+    {
+       k=0;
+      for(j = 0; j < set_size; j++)
+       {
+          /* Check if jth bit in the counter is set
+             If set then print jth element from set */
+          if(counter & (1<<j)){
+            Processing[k] = Set[j];
+            //std::cout << Set[j];
+            k++;
+        }
+
+       }
+       //std::cout << std::endl;
+       Processing.resize(k);
+    }
+}
+
+class BottomUp{
+public:
+    std::vector<int> Set;
+    std::vector<int> Config;
+    std::vector<std::vector<int>> Powerset;
+    int i,size;
+    int loc;
+
+    void Init(std::vector<int> &X);
+    void Fit(std::vector<int> &Node);
+
+
+};
+
+
+void BottomUp::Init(std::vector<int> &X){
+    Set = X;
+    std::vector<int> Empty = {};
+    loc = 0;
+    Powerset.resize(pow(2,X.size()+1));
+    Powerset[loc] = Empty;
+    loc++;
+}
+
+void BottomUp::Fit(std::vector<int> &Node){
+    std::vector<int> Branch = Node;
+    int x = Branch.back();
+    Branch.pop_back();
+
+    if (Branch.size()>0){
+        Fit(Branch);
+    }
+    i=0;
+    size = loc;
+    while(i<size){
+        Config = Powerset[i];
+        Config.push_back(x);
+        Powerset[loc] = Config;
+        loc++;
+        i++;
+    }
+
+}
+
